6ad7c2eb26ca44d61d0704ffa8a10de83e952e08
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-05-26 23:32:17 +0500

    feat: create option component

---
+++ src/components/select/option/Option.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+import './Option.scss';
+
+const Option: FC = () => {
+  return (
+    <li
+    // key={value}
+    // onClick={(e) => handleOption(e, index, disabled)}
+    // className={classList.option(index, disabled)}
+    >
+      {/* {label} */}
+    </li>
+  );
+};
+
+export default Option;
