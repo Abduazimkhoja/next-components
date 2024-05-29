945e94d2b649fd7529347e71c27d001ce60205e9
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-05-29 23:48:18 +0500

    refactor: use searched options

--- src/components/select/menu/Menu.tsx
+++ src/components/select/menu/Menu.tsx
@@ -4,13 +4,13 @@ import Option from '../option/Option';
 import { cn } from '../select-utils';
 
 const SelectMenu: FC = () => {
-  const { isOpen, optionsWithId } = useSelectContext();
+  const { isOpen, optionsWithId, searchOptions } = useSelectContext();
 
   const menuClass = cn('select__menu', isOpen ? 'select__menu-show' : '');
 
   return (
     <ul className={menuClass}>
-      {optionsWithId.map((option) => {
+      {searchOptions.map((option) => {
         return <Option key={option.id} option={option} />;
       })}
     </ul>
