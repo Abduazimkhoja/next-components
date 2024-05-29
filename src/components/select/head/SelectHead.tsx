bf6f24d21ccf216e0688cdee105f52d0bd6ba1ef
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-05-29 23:47:57 +0500

    refactor: select heade add functional

--- src/components/select/head/SelectHead.tsx
+++ src/components/select/head/SelectHead.tsx
@@ -1,11 +1,42 @@
-import { FC } from 'react';
+'use client';
+import { ChangeEvent, FC, MouseEvent, useState } from 'react';
 import ClearButton from '../ClearButton';
+import { useSelectContext } from '../context/SelectContext';
 import Label from '../label/Label';
+import { TSelectOptionWithId } from '../select.type';
 
 const SelectHead: FC = () => {
+  const {
+    picked,
+    isMultiple,
+    toggleDropdown,
+    handelSearchOptions,
+  } = useSelectContext();
+
+  const handleInputClick = (event: MouseEvent<HTMLInputElement>) => {
+    event.stopPropagation();
+    toggleDropdown();
+  };
+
   return (
-    <div>
-      <Label label='label'></Label>
+    <div className='select__head'>
+      <div className='select__labels'>
+        {isMultiple &&
+          picked.map((option) => {
+            return <Label key={option.id} option={option}></Label>;
+          })}
+        <input
+          onChange={handelSearchOptions}
+          onClick={handleInputClick}
+          unselectable='on'
+          readOnly={!isMultiple}
+          // placeholder='Select'
+          type='search'
+          defaultValue={isMultiple ? '' : picked[0].label}
+          autoComplete='off'
+          role='combobox'
+        />
+      </div>
       <ClearButton />
     </div>
   );
