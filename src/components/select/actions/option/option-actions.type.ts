fc54c0f9813d7190c205363c578ff2885760a3dd
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-05-29 23:57:31 +0500

    refactor: remove type TPicked

--- src/components/select/actions/option/option-actions.type.ts
+++ src/components/select/actions/option/option-actions.type.ts
@@ -1,26 +1,26 @@
-import { Dispatch, SetStateAction } from 'react';
-import { TPicked } from '../../container/SelectContainer';
+import { Dispatch, MouseEvent, SetStateAction } from 'react';
+// import { TPicked } from '../../container/SelectContainer';
 import { TSelectOptionWithId } from '../../select.type';
 
 type TDelete = (option: TSelectOptionWithId) => void;
 type TIsSelect = (option: TSelectOptionWithId) => boolean;
 
 type TToggle = (props: {
-  event: MouseEvent;
+  event: MouseEvent<HTMLDivElement | HTMLButtonElement | HTMLLIElement>;
   option: TSelectOptionWithId;
   isMultiple?: boolean;
 }) => void;
 
 export type TOptionActionsObject = {
-  clear: VoidFunction;
+  clear: () => void;
   delete: TDelete;
   toggle: TToggle;
   isSelect: TIsSelect;
 };
 
 type pickState = {
-  picked: TPicked;
-  setPicked: Dispatch<SetStateAction<TPicked>>;
+  picked: TSelectOptionWithId[];
+  setPicked: Dispatch<SetStateAction<TSelectOptionWithId[]>>;
 };
 
 type isOpenState = {
