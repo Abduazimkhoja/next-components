1f5a03632924aa8181558b4b8117d538aa13986b
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-05-28 22:54:23 -1200

    refactor: complete option component

--- src/components/select/option/Option.tsx
+++ src/components/select/option/Option.tsx
@@ -1,14 +1,29 @@
 import { FC } from 'react';
-import './Option.scss';
+import { useSelectContext } from '../context/SelectContext';
+import { TOption } from './option.type';
+import { cn } from '../select-utils';
 
-const Option: FC = () => {
+const Option: FC<TOption> = ({ option }) => {
+  const { optionActions, isMultiple, isOpen } = useSelectContext();
+  const isActive = optionActions.isSelect(option);
+  console.log(isOpen);
+  
+
+  const optionClass = cn(
+    'select__option',
+    isActive ? 'select__option-active' : '',
+    option.disabled ? 'select__option-disabled' : '',
+  );
+
+  const handleClick = (event: React.MouseEvent<HTMLLIElement>) => {
+    optionActions.toggle({ event, option, isMultiple });
+  };
+
   return (
-    <li
-    // key={value}
-    // onClick={(e) => handleOption(e, index, disabled)}
-    // className={classList.option(index, disabled)}
-    >
-      {/* {label} */}
+    <li onClick={handleClick} className={optionClass}>
+      <button disabled={option.disabled}>
+        {option.label}
+      </button>
     </li>
   );
 };
