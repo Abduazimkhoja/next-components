c6c494bfe35106881a72064961dfee5fe4b5b981
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-06-03 10:27:53 +0500

    refactor: add handleClick

--- src/components/select/ClearButton.tsx
+++ src/components/select/ClearButton.tsx
@@ -1,20 +1,22 @@
-import { FC } from 'react';
+import { FC, MouseEvent } from 'react';
 import { useSelectContext } from './context/SelectContext';
 import { cn } from './select-utils';
 
 const ClearButton: FC = () => {
-  const { optionActions } = useSelectContext();
-  const clearButtonClass = cn('select__clear');
-
-  return (
-    <button
-      onClick={(event) => {
-        event.stopPropagation();
-        optionActions.clear();
-      }}
-      className={clearButtonClass}
-    ></button>
+  const { optionActions, isMultiple } = useSelectContext();
+  const clearButtonClass = cn(
+    'select__button',
+    isMultiple ? 'select__button-clear' : 'select__button-search',
   );
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
+    if (isMultiple) {
+      optionActions.clear();
+    }
+  };
+
+  return <button onClick={handleClick} className={clearButtonClass}></button>;
 };
 
 export default ClearButton;
